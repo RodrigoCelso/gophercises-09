4ac1f0729dc902b424754ef1108c0c8c32c6d845
RCBONFIM <RCBONFIM@tce.mt.gov.br>
2025-08-26 16:47:21 -0400

    Bonus completed

--- deck/card.go
+++ deck/card.go
@@ -6,6 +6,8 @@ import (
 	"sort"
 )
 
+// CardSuit represents the suit from the Card (Spade = 0, ...).
+// Joker is considered a suit.
 type CardSuit int
 
 const (
@@ -17,6 +19,7 @@ const (
 	Joker
 )
 
+// CardValue are the value written in the card (Ace=1, ...)
 type CardValue int
 
 const (
@@ -37,6 +40,7 @@ const (
 	valueSize
 )
 
+// Cards are the structures that composes the deck.
 type Card struct {
 	Suit       CardSuit
 	Value      CardValue
@@ -52,7 +56,8 @@ func newSuitDeck(suit CardSuit) []Card {
 	return suitDeck
 }
 
-func New(opts ...Option) *[]Card {
+// New allocates a new slice of Card given the n Options.
+func New(opts ...option) *[]Card {
 	var deck []Card
 	for suit := range suitSize {
 		deck = append(deck, newSuitDeck(CardSuit(suit))...)
@@ -63,9 +68,10 @@ func New(opts ...Option) *[]Card {
 	return &deck
 }
 
-type Option func([]Card) []Card
+type option func([]Card) []Card
 
-func WithJoker(quantity int) Option {
+// WithJoker appends the given quantity of Joker cards.
+func WithJoker(quantity int) option {
 	return func(c []Card) []Card {
 		for idx := 0; idx < quantity; idx++ {
 			card := Card{Suit: Joker, Value: CardValue(0), TrumpValue: 55}
@@ -75,7 +81,8 @@ func WithJoker(quantity int) Option {
 	}
 }
 
-func WithSort() Option {
+// WithSort returns the cards sorted by suit.
+func WithSort() option {
 	return func(c []Card) []Card {
 		sort.Slice(c, func(i, j int) bool {
 			return c[i].Suit < c[j].Suit
@@ -84,14 +91,16 @@ func WithSort() Option {
 	}
 }
 
-func WithSortFunc(less func(cards []Card) func(i, j int) bool) Option {
+// WithSortFunc returns the cards sorted by the given function.
+func WithSortFunc(less func(cards []Card) func(i, j int) bool) option {
 	return func(c []Card) []Card {
 		sort.Slice(c, less(c))
 		return c
 	}
 }
 
-func WithShuffle() Option {
+// WithShuffle uses the rand standard package and returns the shuffled cards.
+func WithShuffle() option {
 	return func(c []Card) []Card {
 		rand.Shuffle(len(c), func(i, j int) {
 			c[i], c[j] = c[j], c[i]
@@ -100,7 +109,8 @@ func WithShuffle() Option {
 	}
 }
 
-func WithFilter(cardFilter func(card Card) bool) Option {
+// WithFilter returns the cards without the filtered cards by the given function.
+func WithFilter(cardFilter func(card Card) bool) option {
 	return func(cards []Card) []Card {
 		var filteredDeck []Card
 		for _, card := range cards {
@@ -112,7 +122,8 @@ func WithFilter(cardFilter func(card Card) bool) Option {
 	}
 }
 
-func WithMultipleDeck(decks ...[]Card) Option {
+// WithMultipleDeck returns a new deck composed by the given decks.
+func WithMultipleDeck(decks ...[]Card) option {
 	return func(c []Card) []Card {
 		var multipleDeck []Card
 		for _, deck := range decks {
--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 )
 
 func main() {
-	deckSort := deck.New(deck.WithJoker(2), deck.WithSort())
+	// New Deck Example
+	deckNew := deck.New()
+	// Deck with Default Sort Example
+	deckSort := deck.New(deck.WithSort())
+	// Deck with Sort Example
 	deckSortFunc := deck.New(
 		deck.WithJoker(1),
 		deck.WithSortFunc(func(cards []deck.Card) func(i int, j int) bool {
@@ -15,17 +19,30 @@ func main() {
 				return cards[i].Value < cards[j].Value
 			}
 		}))
+	// Deck with Joker Example
+	deckJoker := deck.New(deck.WithJoker(3))
+	// Deck with Shuffle Example
 	deckShuffle := deck.New(deck.WithShuffle())
+	// Deck with Filter Example
 	deckFilter := deck.New(
 		deck.WithJoker(5),
 		deck.WithFilter(func(card deck.Card) bool {
 			return card.Value == deck.Jack || card.Suit == deck.Joker
 		}))
+	// Deck with Multiple Decks Example
 	deckMultiple := deck.New(deck.WithMultipleDeck(*deckSort, *deckSortFunc, *deckShuffle, *deckFilter), deck.WithSort())
 
-	fmt.Println("Deck Sort:", deckSort)
-	fmt.Println("Deck Sort Func:", deckSortFunc)
-	fmt.Println("Deck Shuffle:", deckShuffle)
-	fmt.Println("Deck Filter:", deckFilter)
-	fmt.Println("Deck Multiple:", deckMultiple)
+	fmt.Println("New Deck:", deckNew)
+	fmt.Println("================================================================================================================")
+	fmt.Println("Deck With Sort:", deckSort)
+	fmt.Println("================================================================================================================")
+	fmt.Println("Deck With Sort Func:", deckSortFunc)
+	fmt.Println("================================================================================================================")
+	fmt.Println("Deck With Joker", deckJoker)
+	fmt.Println("================================================================================================================")
+	fmt.Println("Deck With Shuffle:", deckShuffle)
+	fmt.Println("================================================================================================================")
+	fmt.Println("Deck With Filter:", deckFilter)
+	fmt.Println("================================================================================================================")
+	fmt.Println("Deck With Multiple:", deckMultiple)
 }
